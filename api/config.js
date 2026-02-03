0ea4863302a5b30905907c9888d3c004ece5b64c
MB <senior.mishel@gmail.com>
2026-02-03 21:42:43 +0200

    add config from back

--- api/config.js
+++ api/config.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
+
     res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
 
     if (req.method === "OPTIONS") {
