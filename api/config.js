57419110b5bb88991b55bab4f165427a65bdb272
MB <senior.mishel@gmail.com>
2026-02-03 21:17:13 +0200

    add config from back

---
+++ api/config.js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
+    res.status(200).json({
+        eventTypeId: process.env.CAL_EVENT_TYPE_ID
+            ? Number(process.env.CAL_EVENT_TYPE_ID)
+            : null,
+
+        eventTypeSlug: process.env.CAL_EVENT_TYPE_SLUG || null,
+        username: process.env.CAL_USERNAME || null,
+
+        defaultDuration: 60,
+        timeZone: "Europe/Kiev",
+    });
+}
